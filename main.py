6c296d9ec35fcfbf05f755f13fb9d5facc8d6341
angel <buildingwill@gmail.com>
2020-12-20 00:17:49 -0500

    fix for openapi json bug

--- main.py
+++ main.py
@@ -9,19 +9,18 @@ app = fastapi.FastAPI()
 
 try:
     db = DB(dbname=dbname, host=dbhost, port=dbport, user=dbuser, passwd=dbpassword)
-
 except:
     print("db error")
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Oh Hell World"}
 
 
 @app.post("/input")
-def read_data(device_id: str, temp=float, humid=float, light=float, ):
-    # data = request.data  # data is empty
+def read_data(device_id: str, temp=float, humid=float, light=float ):
+
     db = DB(dbname=dbname, host=dbhost, port=dbport, user=dbuser, passwd=dbpassword)
     table = "raw_data"
     local_received_time = datetime.datetime.utcnow()
@@ -36,20 +35,20 @@ def read_data(device_id: str, temp=float, humid=float, light=float, ):
     return ("received")
 
 
-@app.get("/test")
-def test_db_conn():
-
-    device_id = "test_device_id"
-    temp = "25.20"
-    humid = "31.00"
-    light = "0.10"
-    local_received_time = time.time()
-    table = "raw_data"
-    conn = psycopg2.connect(dbname=dbname, user=dbuser, password=dbpassword, port=dbport, host=dbhost)
-    insert_statement = f"""INSERT INTO {table} (device_id,temp,humid,light,local_received_time)
-                        VALUES ({device_id},{temp},{humid},{light},{local_received_time});
-                        """
-    return (conn)
+# @app.get("/test")
+# def test_db_conn():
+#
+#     device_id = "test_device_id"
+#     temp = "25.20"
+#     humid = "31.00"
+#     light = "0.10"
+#     local_received_time = time.time()
+#     table = "raw_data"
+#     conn = psycopg2.connect(dbname=dbname, user=dbuser, password=dbpassword, port=dbport, host=dbhost)
+#     insert_statement = f"""INSERT INTO {table} (device_id,temp,humid,light,local_received_time)
+#                         VALUES ({device_id},{temp},{humid},{light},{local_received_time});
+#                         """
+#     return (conn)
 
 
 if __name__ == "__main__":
